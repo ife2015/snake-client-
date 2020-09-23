db5fe162082054dd35580a8a47947a25b6f0dfc3
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-23 21:50:48 +0000

    snake game update 9

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require('net'); 
+const {IP, PORT} = require(`./constant.js`);
 
 const connect = function () {
   const conn = net.createConnection({
-    host: "135.23.222.131" , //135.23.222.131
-    port: 50542
+    host: IP , //135.23.222.131
+    port: PORT
   });
 
   conn.setEncoding('utf8'); 
