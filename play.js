9e95fdd84b9f6eb9c4b1b2eff28f9459da8475a6
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-23 20:12:58 +0000

    snake update 4

--- play.js
+++ play.js
@@ -2,5 +2,23 @@
 const {connect} = require(`./client.js`); 
 
 console.log('Connecting ...'); 
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+
+  stdin.on('data', (data)=> {
+    //if hits control + c
+    if(data === '\\q\n') {
+      process.exit();
+    }
+  }); 
+
+  stdin.resume();
+  return stdin;
+}
+
 connect(); 
+setupInput();
 
